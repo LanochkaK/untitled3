7061f75bdb311bf9e9f65a0e9060de08a11e8c1d
sviatlanaCydeo <ramashka1881@gmail.com>
2023-05-10 18:53:55 -0400

    first part is created

---
+++ src/file1/Part1.java
@@ -0,0 +1,14 @@
+package file1;
+
+public class Part1 {
+
+    // new project is created
+
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+
+
+
+}
